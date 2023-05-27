85e1b03ca69aa2dc06389abd6d8ec57377f84dc6
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-05-28 02:34:07 +0300

    Fix og

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Seçim çetele v3",
   description: "Seçim çetele v3",
+  openGraph: {
+    title: "Seçim çetele v3",
+    description: "Seçim çetele v3",
+    image: `${
+      process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+    }/api/og`,
+  },
 };
 
 export default function RootLayout({
@@ -15,25 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="light">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta
-          name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
-            }/api/og`
-          }
-        />
-      </Head>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
