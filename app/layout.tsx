d2a261624b7fafdd67467e4a967d994cd66a579b
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-05-28 02:41:12 +0300

    fix og

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,20 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
+const openGraphImage = {
+  images: [
+    `${
+      process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+    }/api/og`,
+  ],
+};
 export const metadata = {
   title: "Seçim çetele v3",
   description: "Seçim çetele v3",
   openGraph: {
+    ...openGraphImage,
     title: "Seçim çetele v3",
     description: "Seçim çetele v3",
-    image: `${
-      process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
-    }/api/og`,
   },
 };
 
