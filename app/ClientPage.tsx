86e4448b8b5e33ae42258aaacb851766dd4140e7
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-05-27 19:12:21 +0300

    add

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Cell from "./components/Cell";
+import { useEffect, useState } from "react";
 import { Space } from "react-zoomable-ui";
+import Cell from "./components/Cell";
 
 const ClientPage = () => {
+  const [invalidVotes, setInvalidVotes] = useState("0");
   const { width, height } = { width: 200, height: 520 };
   const createGrid = (name: string) => {
     return Array.from({ length: 40 }).map((_, i) =>
@@ -16,6 +17,19 @@ const ClientPage = () => {
   };
   const [tayyipVotes, setTayyipVotes] = useState(createGrid("tayyip"));
   const [kemalVotes, setKemalVotes] = useState(createGrid("kemal"));
+
+  useEffect(() => {
+    if (localStorage.getItem("tayyipVotes")) {
+      setTayyipVotes(JSON.parse(localStorage.getItem("tayyipVotes")!));
+    } else {
+      setTayyipVotes(createGrid("tayyip"));
+    }
+    if (localStorage.getItem("kemalVotes")) {
+      setKemalVotes(JSON.parse(localStorage.getItem("kemalVotes")!));
+    } else {
+      setKemalVotes(createGrid("kemal"));
+    }
+  }, []);
 
   const handleClick = (e: any) => {
     const target = e.target;
@@ -32,6 +46,7 @@ const ClientPage = () => {
           if (cell.id === id) {
             cell.active = !cell.active;
             setTayyipVotes([...tayyipVotes]);
+            localStorage.setItem("tayyipVotes", JSON.stringify(tayyipVotes));
             return;
           }
         }
@@ -44,6 +59,7 @@ const ClientPage = () => {
           if (cell.id === id) {
             cell.active = !cell.active;
             setKemalVotes([...kemalVotes]);
+            localStorage.setItem("kemalVotes", JSON.stringify(kemalVotes));
             return;
           }
         }
@@ -52,67 +68,89 @@ const ClientPage = () => {
   };
 
   return (
-    <div className="flex gap-6">
-      <div style={{ width: width, height: height, position: "relative" }}>
-        <Space style={{ width: width, height: height }}>
-          <table
-            className="border-collapse border-[0.5px] border-slate-500 "
-            onClick={handleClick}
-            cellPadding={0}
-            cellSpacing={0}
-          >
-            <thead>
-              <tr>
-                <th className="border-[0.5px] border-slate-600" colSpan={10}>
-                  RECEP TAYYIP ERDOĞAN
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {tayyipVotes.map((row, i) => (
-                <tr key={i}>
-                  {row.map((cell) => (
-                    <Cell key={cell.id} id={cell.id} active={cell.active}>
-                      {cell.text}
-                    </Cell>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </Space>
+    <main>
+      <div className="flex gap-3">
+        <span className="w-full text-center">
+          RECEP
+          <br />
+          TAYYİP ERDOĞAN
+        </span>
+        <span className="w-full text-center">
+          KEMAL
+          <br />
+          KILIÇDAROĞLU
+        </span>
+      </div>
+      <div className="flex gap-3">
+        <div style={{ width: "100%", height: height, position: "relative" }}>
+          <Space style={{ height: height }}>
+            <table
+              width={"100%"}
+              className="border-collapse border-[0.5px] border-slate-500 "
+              onClick={handleClick}
+              cellPadding={0}
+              cellSpacing={0}
+            >
+              <tbody>
+                {tayyipVotes.map((row, i) => (
+                  <tr key={i}>
+                    {row.map((cell) => (
+                      <Cell key={cell.id} id={cell.id} active={cell.active}>
+                        {cell.text}
+                      </Cell>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Space>
+        </div>
+
+        <div style={{ width: "100%", height: height, position: "relative" }}>
+          <Space style={{ height: height }}>
+            <table
+              width={"100%"}
+              className="border-collapse border-[0.5px] border-slate-500 "
+              onClick={handleClick}
+              cellPadding={0}
+              cellSpacing={0}
+            >
+              <tbody>
+                {kemalVotes.map((row, i) => (
+                  <tr key={i}>
+                    {row.map((cell) => (
+                      <Cell key={cell.id} id={cell.id} active={cell.active}>
+                        {cell.text}
+                      </Cell>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Space>
+        </div>
+      </div>
+      <div className="flex gap-3">
+        <span className="w-full text-center">
+          Toplam: {tayyipVotes.flat().filter((c) => c.active).length}
+        </span>
+        <span className="w-full text-center">
+          Toplam: {kemalVotes.flat().filter((c) => c.active).length}
+        </span>
       </div>
 
-      <div style={{ width: width, height: height, position: "relative" }}>
-        <Space style={{ width: width, height: height }}>
-          <table
-            className="border-collapse border-[0.5px] border-slate-500 "
-            onClick={handleClick}
-            cellPadding={0}
-            cellSpacing={0}
-          >
-            <thead>
-              <tr>
-                <th className="border-[0.5px] border-slate-600" colSpan={10}>
-                  KEMAL KILIÇDAROĞLU
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {kemalVotes.map((row, i) => (
-                <tr key={i}>
-                  {row.map((cell) => (
-                    <Cell key={cell.id} id={cell.id} active={cell.active}>
-                      {cell.text}
-                    </Cell>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </Space>
+      <div className="flex gap-3">
+        <span className="w-full text-center">
+          Geçersiz oy:{" "}
+          <input
+            value={invalidVotes}
+            onChange={(e) => setInvalidVotes(e.target.value.toString())}
+            type="tel"
+            className="w-12 h-8 text-center text-blue-500"
+          />
+        </span>
       </div>
-    </div>
+    </main>
   );
 };
 
