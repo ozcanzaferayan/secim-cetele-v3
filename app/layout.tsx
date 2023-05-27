3505d0ff670066cfd1626c049596d8a5f05d605a
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-05-27 23:12:16 +0300

    Fix og:img

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,18 @@ export default function RootLayout({
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/api/og" />
+        <meta
+          name="og:image"
+          content={
+            // Because OG images must have a absolute URL, we use the
+            // `VERCEL_URL` environment variable to get the deployment’s URL.
+            // More info:
+            // https://vercel.com/docs/concepts/projects/environment-variables
+            `${
+              process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
+            }/api/og`
+          }
+        />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
