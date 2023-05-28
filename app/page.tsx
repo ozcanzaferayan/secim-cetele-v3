2fbf51f11f3fad61aabf4dff1d1f6d5879708b67
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-05-28 14:55:33 +0300

    Add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import ClientPage from "./ClientPage";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
-  return <ClientPage />;
+  return (
+    <>
+      <ClientPage />
+      <Analytics />
+    </>
+  );
 }
